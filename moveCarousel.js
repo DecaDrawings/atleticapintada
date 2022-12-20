e5d7968e879578b38b3f9b4680e8b03ae0d23fc3
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-20 00:55:32 -0400

    k

--- moveCarousel.js
+++ moveCarousel.js
@@ -1,3 +1,3 @@
 var nav = document.querySelector("nav");
 var navHeight = nav.offsetHeight;
-document.getElementsByClassName("carrossel")[0].style.paddingTop = navHeight + 14 +  "px";
+document.getElementsByClassName("carrossel")[0].style.paddingTop = navHeight + 18 +  "px";
