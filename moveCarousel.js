930c51517ed885d95e457005c2116f557d13bbb7
Guilherme Garcia <guilherme.r@ufms.br>
2022-12-20 00:41:06 -0400

    K

--- moveCarousel.js
+++ moveCarousel.js
@@ -1,3 +1,3 @@
 var nav = document.querySelector("nav");
 var navHeight = nav.offsetHeight;
-document.getElementsByClassName("container-fluid")[0].style.marginTop = navHeight;
+document.getElementsByClassName("container-fluid")[0].style.marginTop = navHeight + "px";
